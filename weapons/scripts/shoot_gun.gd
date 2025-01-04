9ef2c013191ddd80f299c86adf6fe78244ddfe28
Rain <raingayming@gmail.com>
2025-01-04 14:52:30 +0000

    shotgun audio added

--- weapons/scripts/shoot_gun.gd
+++ weapons/scripts/shoot_gun.gd
@@ -3,6 +3,10 @@ extends Node3D
 @export_group("refereences")
 @export var anim: AnimatedSprite3D
 
+@export_group("audio")
+@export var shoot_effects: Array[AudioStreamPlayer3D]
+@export var reload_effect: AudioStreamPlayer3D
+
 @export_group("shooting")
 @export var shoot_cooldown: float
 @export var shoot_timer: float
@@ -18,20 +22,26 @@ func _ready():
 	anim.play(anim_name + "_idle")
 
 func _process(delta):
-	if shoot_cooldown > 0:
-		shoot_cooldown -= delta
+	if shoot_timer > 0:
+		shoot_timer -= delta
 
-	if Input.is_action_just_pressed("combat_shoot") and shoot_cooldown <= 0:
+	if Input.is_action_just_pressed("combat_shoot") and shoot_timer <= 0:
 		shoot()
 	
 	#if the shooting animation is finished, play the idle animation
 	if anim.animation == anim_name + "_shoot":
 		if anim.frame == 12:
 			anim.play(anim_name + "_idle")
+		
+		if anim.frame == 3 and reload_effect != null:
+			reload_effect.play()
 
 func shoot():
 	shoot_timer = shoot_cooldown
 	anim.play(anim_name + "_shoot")
+
+	var random_play = randi_range(0, shoot_effects.size() - 1)
+	shoot_effects[random_play].play()
 
 	#raycasting
 	if shoot_from.is_colliding():
